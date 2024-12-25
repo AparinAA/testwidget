a743ac9fc59bde21933ffeec9d1acdb3324b7214
Александр Апарин <aparinalx@yandex.ru>
2024-12-25 11:40:29 +0100

    test

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,29 @@ import { useState } from "react";
 import styles from "./App.module.css";
 
 function App() {
-	const [show, setShow] = useState(false);
+	const [show, setShow] = useState("");
 
 	return (
 		<div className={styles.main}>
 			{show && (
 				<iframe
 					src="https://www.aleksandraparin.site/"
-					className={styles.frameChat}
+					className={[styles.frameChat, show && styles[show]].join(
+						" "
+					)}
 				></iframe>
 			)}
-			<button onClick={() => setShow((on) => !on)}>Show widget</button>
+			<div className={styles.actionMenu}>
+				<button onClick={() => setShow("top")}>
+					Show widget on top
+				</button>
+				<button onClick={() => setShow("bottom")}>
+					Show widget on bottom
+				</button>
+				<button onClick={() => setShow("center")}>
+					Show widget on center
+				</button>
+			</div>
 		</div>
 	);
 }
